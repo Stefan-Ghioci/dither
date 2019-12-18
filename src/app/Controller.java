7ceee273f2a0ba5ea5710df92a79a1c34f360345
Stefan Ghioci <ghioci.stefan@gmail.com>
2019-12-18 20:46:07 +0200

    Basic Scene layout

---
+++ src/app/Controller.java
@@ -0,0 +1,25 @@
+package app;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+
+public class Controller {
+    public ImageView originalImageView;
+    public ImageView editedImageView;
+    public ColorPicker colorPicker;
+    public ListView paletteListView;
+
+    public void loadImage(ActionEvent actionEvent) {
+
+    }
+
+    public void addColorToList(ActionEvent actionEvent) {
+
+    }
+
+    public void saveImage(ActionEvent actionEvent) {
+
+    }
+}
