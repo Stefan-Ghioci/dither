7fafabca1a09e21d1fa8ae8c2afba970944fbef7
Stefan Ghioci <ghioci.stefan@gmail.com>
2019-12-30 19:08:43 +0200

    Change dither to use quantization for color palette

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -1,25 +1,155 @@
 package app;
 
-import javafx.event.ActionEvent;
-import javafx.scene.control.ColorPicker;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Insets;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.image.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.util.Callback;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.lang.Math.pow;
 
 public class Controller {
+    public AnchorPane root;
     public ImageView originalImageView;
     public ImageView editedImageView;
-    public ColorPicker colorPicker;
-    public ListView paletteListView;
+    public ListView<Color> paletteListView;
+    public Spinner factorSpinner;
+    FileChooser fileChooser = new FileChooser();
 
-    public void loadImage(ActionEvent actionEvent) {
+    //TODO: refactor color palette list view into pure rectangles
+    public void initialize() {
+        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Images", "*.png", "*.bmp", "*.jpg");
+        fileChooser.getExtensionFilters().add(filter);
+
+        paletteListView.setCellFactory(new Callback<>() {
+            @Override
+            public ListCell<Color> call(ListView<Color> colorListView) {
+                return new ListCell<>() {
+                    @Override
+                    protected void updateItem(Color color, boolean empty) {
+                        super.updateItem(color, empty);
+
+                        if (color != null) {
+                            setBackground(new Background(new BackgroundFill(color, CornerRadii.EMPTY, Insets.EMPTY)));
+                            int paletteSize = 2;
+                            if ((int) factorSpinner.getValue() != 0)
+                                paletteSize = (int) (pow((int) factorSpinner.getValue() + 1, 3));
+                            setPrefWidth(colorListView.getWidth() / paletteSize);
+                        } else {
+                            setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+                        }
+                    }
+
+                };
+            }
+        });
+
+        factorSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 256));
+        factorSpinner.setOnMouseClicked(mouseEvent -> {
+            int factor = (int) factorSpinner.getValue();
+            paletteListView.getItems().clear();
+            if (factor == 0) {
+                paletteListView.getItems().add(Color.BLACK);
+                paletteListView.getItems().add(Color.WHITE);
+            } else
+                for (int x = 0; x <= factor; x++)
+                    for (int y = 0; y <= factor; y++)
+                        for (int z = 0; z <= factor; z++)
+                            paletteListView.getItems().add(new Color((double) x / factor, (double) y / factor, (double) z / factor, 1.0));
+
+        });
+        factorSpinner.getOnMouseClicked().handle(null);
 
     }
 
-    public void addColorToList(ActionEvent actionEvent) {
-
+    public void loadImage() {
+        File file = fileChooser.showOpenDialog(root.getScene().getWindow());
+        if (file != null) {
+            Image image = new Image(file.toURI().toString());
+            originalImageView.setImage(image);
+        }
     }
 
-    public void saveImage(ActionEvent actionEvent) {
 
+    public void saveImage() throws IOException {
+        File file = fileChooser.showSaveDialog(root.getScene().getWindow());
+        if (file != null) {
+            String name = file.getName();
+            String extension = name.substring(1 + name.lastIndexOf(".")).toLowerCase();
+            ImageIO.write(SwingFXUtils.fromFXImage(editedImageView.getImage(), null), extension, file);
+        }
     }
+
+    public void applyDither() {
+        Image image = originalImageView.getImage();
+        PixelReader pixelReader = image.getPixelReader();
+
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+
+        Color[][] pixelMatrix = new Color[width][height];
+
+        WritableImage writableImage = new WritableImage(width, height);
+        PixelWriter pixelWriter = writableImage.getPixelWriter();
+
+        for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++)
+                pixelMatrix[x][y] = pixelReader.getColor(x, y);
+
+        List<Color> palette = paletteListView.getItems();
+        palette.sort(Comparator.comparing(Color::toString));
+
+        for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++) {
+                Color oldColor = pixelMatrix[x][y];
+
+                Color newColor = Processing.quantize(oldColor, (Integer) factorSpinner.getValue());
+
+                pixelMatrix[x][y] = newColor;
+
+                double redError = oldColor.getRed() - newColor.getRed();
+                double greenError = oldColor.getGreen() - newColor.getGreen();
+                double blueError = oldColor.getBlue() - newColor.getBlue();
+
+                try {
+                    pixelMatrix[x + 1][y] = Processing.addErrorToColor(pixelMatrix[x + 1][y], redError, greenError, blueError, 7.0 / 16);
+                } catch (IndexOutOfBoundsException ignored) {
+                }
+                try {
+                    pixelMatrix[x - 1][y + 1] = Processing.addErrorToColor(pixelMatrix[x - 1][y + 1], redError, greenError, blueError, 7.0 / 16);
+                } catch (IndexOutOfBoundsException ignored) {
+                }
+                try {
+                    pixelMatrix[x][y + 1] = Processing.addErrorToColor(pixelMatrix[x][y + 1], redError, greenError, blueError, 3.0 / 16);
+                } catch (IndexOutOfBoundsException ignored) {
+                }
+                try {
+                    pixelMatrix[x + 1][y + 1] = Processing.addErrorToColor(pixelMatrix[x + 1][y + 1], redError, greenError, blueError, 5.0 / 16);
+                } catch (IndexOutOfBoundsException ignored) {
+                }
+
+            }
+
+        for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++)
+                pixelWriter.setColor(x, y, pixelMatrix[x][y]);
+
+        editedImageView.setImage(writableImage);
+    }
+
 }
---
+++ src/app/Processing.java
@@ -0,0 +1,29 @@
+package app;
+
+import javafx.scene.paint.Color;
+
+public class Processing {
+
+
+    public static Color addErrorToColor(Color color, double redError, double greenError, double blueError, double x) {
+        return new Color(
+                Math.min(1.0, Math.max(0.0, color.getRed() + redError * x)),
+                Math.min(1.0, Math.max(0.0, color.getGreen() + greenError * x)),
+                Math.min(1.0, Math.max(0.0, color.getBlue() + blueError * x)),
+                color.getOpacity());
+    }
+
+    static Color quantize(Color color, int factor) {
+        if (factor == 0) {
+            double gray = color.getRed() * 0.3 + color.getGreen() * 0.59 + color.getBlue() * 0.11;
+            double bw = Math.round(gray);
+            return new Color(bw, bw, bw, color.getOpacity());
+        }
+
+        double red = (double) (Math.round(factor * color.getRed())) / factor;
+        double green = (double) (Math.round(factor * color.getGreen())) / factor;
+        double blue = (double) (Math.round(factor * color.getBlue())) / factor;
+
+        return new Color(red, green, blue, color.getOpacity());
+    }
+}
